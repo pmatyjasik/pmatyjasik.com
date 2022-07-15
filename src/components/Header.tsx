3d9d5d5ffdf71821f9a344a8425b5cb67a213288
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-16 01:01:36 +0200

    Add Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Heading from "components/Heading";
+import NavMenu from "components/NavMenu";
+import Bubble from "components/Bubble";
+
+
+const Header = () => {
+  return (
+    <>
+        <header className="fixed top-0 w-full px-4 py-5">
+            <div className="container flex items-center justify-between mx-auto">
+                <Heading heading='Piotr Matyjasik' />
+                <NavMenu/>
+            </div>
+            <Bubble/>
+        </header>
+      </>
+  );
+}
+
+
+export default Header;
