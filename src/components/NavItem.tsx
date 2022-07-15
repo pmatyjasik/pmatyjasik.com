35d678a0e617c2672ac5d1811c41df12f16b1912
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-16 00:58:56 +0200

    Add NavItem component

---
+++ src/components/NavItem.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface Props{
+    url: string;
+    text: string;
+    isOpen?: boolean
+}
+
+
+const NavItem = ({url, text, isOpen=false}:Props) => {
+  return (
+    <li className={`text-white ${isOpen ? '' : 'mb-10'}`}><a href={url}>{text}</a></li>
+  );
+}
+
+
+export default NavItem;
