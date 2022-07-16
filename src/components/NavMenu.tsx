f4ee4fee7149431b33574cd7a126de2a2fffb981
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-16 10:06:59 +0200

    Set state with prevState && change props in navItem

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -9,15 +9,15 @@ const NavMenu = () => {
     <div className="md:w-2/5 xl:w-1/3">
       <nav className="hidden md:block">
         <ul className="flex justify-around text-white">
-          <NavItem isOpen={true} url='#' text='O mnie'/>
-          <NavItem isOpen={true} url='#' text='Technologie'/>
-          <NavItem isOpen={true} url='#' text='Portfolio'/>
+          <NavItem url='#' text='O mnie'/>
+          <NavItem url='#' text='Technologie'/>
+          <NavItem url='#' text='Portfolio'/>
           <NavItem url='#' text='Kontakty'/>
         </ul>
       </nav>
       <FaBars
       className={`z-50 w-8 h-8 text-white cursor-pointer md:hidden`}
-      onClick={() => setIsOpen(!isOpen)}
+      onClick={() => setIsOpen(prev => !prev)}
       />
       <MobileMenu isOpen={isOpen} />
     </div>
