9c9d8930e0cedbc35fce4ce32caa8d2b9e6e4338
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-17 00:11:32 +0200

    Change import LogoName

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Heading from "components/Heading";
+import LogoName from "components/LogoName";
 import NavMenu from "components/NavMenu";
 import Bubble from "components/Bubble";
 
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className="fixed top-0 w-full px-4 py-5">
         <div className="container flex items-center justify-between mx-auto">
-            <Heading heading='Piotr Matyjasik' />
+            <LogoName heading='Piotr Matyjasik' />
             <NavMenu/>
         </div>
         <Bubble/>
