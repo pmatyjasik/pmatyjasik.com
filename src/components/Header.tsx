9ffcbf5d5abf5e6477ed196ba231669f0d08b1cb
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-16 09:56:56 +0200

    Remove jsx wrapper

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,15 +6,13 @@ import Bubble from "components/Bubble";
 
 const Header = () => {
   return (
-    <>
-        <header className="fixed top-0 w-full px-4 py-5">
-            <div className="container flex items-center justify-between mx-auto">
-                <Heading heading='Piotr Matyjasik' />
-                <NavMenu/>
-            </div>
-            <Bubble/>
-        </header>
-      </>
+    <header className="fixed top-0 w-full px-4 py-5">
+        <div className="container flex items-center justify-between mx-auto">
+            <Heading heading='Piotr Matyjasik' />
+            <NavMenu/>
+        </div>
+        <Bubble/>
+    </header>
   );
 }
 
