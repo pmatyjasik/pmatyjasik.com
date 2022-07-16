5e44be1290f1fb4e7c814b45a0503a1a1b397525
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-16 09:53:35 +0200

    Add props to NavItem && move variants up

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -5,11 +5,11 @@ interface Props{
     isOpen: boolean;
 }
 
+const variants = {
+  open: { x: 0, transition: { type: 'linear' } },
+  closed: { x: '100%', transition: { type: 'linear' } },
+};
 const MobileMenu = ({ isOpen }:Props) => {
-  const variants = {
-    open: { x: 0, transition: { type: 'linear' } },
-    closed: { x: '100%', transition: { type: 'linear' } },
-  };
   return (
     <motion.div
       animate={isOpen ? 'open' : 'closed'}
@@ -19,10 +19,10 @@ const MobileMenu = ({ isOpen }:Props) => {
       className='fixed right-0 z-10 flex flex-col items-center w-2/5 rounded-bl-[200px] rounded-tl-[50px] bg-primary top-15 md:hidden'
     >
         <ul className='py-10'>
-        <NavItem url='#' text='O mnie'/>
-        <NavItem url='#' text='Technologie'/>
-        <NavItem url='#' text='Portfolio'/>
-        <NavItem url='#' text='Kontakty'/>
+        <NavItem isOpen={true} url='#' text='O mnie'/>
+        <NavItem isOpen={true} url='#' text='Technologie'/>
+        <NavItem isOpen={true} url='#' text='Portfolio'/>
+        <NavItem isOpen={true} url='#' text='Kontakty'/>
 
         </ul>
     </motion.div>
