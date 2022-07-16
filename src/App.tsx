bca47b0f1db1c6c568c51ea9b4679a86894a5da5
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-17 00:16:30 +0200

    Add wrapper && about

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Header from 'components/Header';
+import SectionWrapper from 'components/SectionWrapper';
+import About from 'components/About';
 
 
 function App() {
   return (
+    <>
     <Header/>
+    <SectionWrapper>
+      <About/>
+    </SectionWrapper>
+    </>
   );
 }
 
