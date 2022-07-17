c1cb8e77960c502b2c2fb0af6d0c494610d6793d
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-17 23:26:33 +0200

    Change formating

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 
-interface Props{
-    url: string;
-    text: string;
-    isOpen?: boolean
+interface Props {
+  url: string;
+  text: string;
+  isOpen?: boolean;
 }
 
-
-const NavItem = ({url, text, isOpen=false}:Props) => ( <li className={`text-white ${isOpen ? 'mb-10' : ''}`}><a href={url}>{text}</a></li> )
-
-
+const NavItem = ({ url, text, isOpen = false }: Props) => (
+  <li className={`text-white ${isOpen ? "mb-10" : ""}`}>
+    <a href={url}>{text}</a>
+  </li>
+);
 
 export default NavItem;
