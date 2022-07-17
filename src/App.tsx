4c470ff8fd334c00f7f7406b6b62219764589a43
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-17 14:15:17 +0200

    Add component: About, Contact, Technologies

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,21 @@ import React from 'react';
 import Header from 'components/Header';
 import SectionWrapper from 'components/SectionWrapper';
 import About from 'components/About';
+import Technologies from 'components/Technologies';
+import Contact from 'components/Contact';
+import Footer from 'components/Footer';
 
 
 function App() {
   return (
     <>
-    <Header/>
-    <SectionWrapper>
-      <About/>
-    </SectionWrapper>
+      <Header/>
+      <SectionWrapper>
+        <About/>
+        <Technologies/>
+        <Contact/>
+      </SectionWrapper>
+      <Footer/>
     </>
   );
 }
