18cb596da23bc31a562daeb9f1fbb4627667f682
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-17 14:11:50 +0200

    Add component to whole footer section

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="p-2 text-center text-white bg-primary">
+      <p>
+        &copy; {new Date().getFullYear()} -{" "}
+        <a href="https://www.linkedin.com/in/piotr-matyjasik/" rel="noopener noreferrer" target="_blank">
+          Piotr Matyjasik [pmatyjasik@icloud.com]
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
