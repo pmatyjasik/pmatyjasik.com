fb39f60f81fbb23137c8ad3614c10f8602b5e407
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-17 23:27:05 +0200

    Refactor

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -1,28 +1,30 @@
 import React, { useState } from "react";
-import NavItem from 'components/NavItem';
+import NavItem from "components/NavItem";
 import { FaBars } from "react-icons/fa";
 import MobileMenu from "components/MobileMenu";
 
 const NavMenu = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const toggle = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
-    <div className="md:w-[45%] lg:w-1/3">
+    <div className="md:w-[45%] lg:w-1/3 m-20:w-1/4">
       <nav className="hidden md:block">
         <ul className="flex justify-around text-white">
-          <NavItem url='#about' text='About'/>
-          <NavItem url='#technologies' text='Technologies'/>
-          <NavItem url='#' text='Portfolio'/>
-          <NavItem url='#contact' text='Contact'/>
+          <NavItem url="#about" text="About" />
+          <NavItem url="#technologies" text="Technologies" />
+          <NavItem url="#" text="Portfolio" />
+          <NavItem url="#contact" text="Contact" />
         </ul>
       </nav>
       <FaBars
-      className={`z-50 w-8 h-8 text-white cursor-pointer md:hidden`}
-      onClick={() => setIsOpen(prev => !prev)}
+        className={`z-50 w-8 h-8 text-white cursor-pointer md:hidden`}
+        onClick={toggle}
       />
       <MobileMenu isOpen={isOpen} />
     </div>
   );
-}
-
+};
 
 export default NavMenu;
