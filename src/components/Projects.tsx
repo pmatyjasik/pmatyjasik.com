414b19b61bf034d83d1ea67ae93379e089f9691d
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-19 22:50:47 +0200

    Display projects from .json

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import SectionHeading from "components/SectionHeading";
-import EmployeeIMG from "assets/employee-pulse.png";
-import QuizationIMG from "assets/quization.png";
-import BookingIMG from "assets/booking.png";
-import PmatyjasikIMG from "assets/pmatyjasik.png";
 import ProjectItem from "components/ProjectItem";
+import ProjectsData from "data/projects.json";
 
 const Projects = () => {
   return (
@@ -14,35 +11,17 @@ const Projects = () => {
         subheading="PORTFOLIO"
         heading="Last Projects"
       />
-      <ProjectItem
-        imageSrc={PmatyjasikIMG}
-        projectTitle="pmatyjasik.com"
-        projectDescription="The page you are currently on is my business card. Technologies used: React.js, Typescript, TailwindCSS, GitHub"
-        projectLink="https://www.pmatyjasik.com/"
-        githubLink="https://github.com/pmatyjasik/pmatyjasik.com"
-        disabled={true}
-      />
-      <ProjectItem
-        imageSrc={EmployeeIMG}
-        projectTitle="Employee Pulse"
-        projectDescription="Project carried out at the University in a group of 4 people. Technologies used: React.js, Typescript, TailwindCSS, GitHub, Firebase. In the final phase made migrations from React.js to Next.js"
-        projectLink="https://employee-pulse.vercel.app/"
-        githubLink="https://github.com/pmatyjasik/Employee-pulse"
-      />
-      <ProjectItem
-        imageSrc={QuizationIMG}
-        projectTitle="Quization"
-        projectDescription="Project carried out at the University in a group of 4 people.  Technologies used: Next.js, Typescript, TailwindCSS, GitHub, Firebase."
-        projectLink="https://quization.vercel.app/"
-        githubLink="https://github.com/pmatyjasik/quization"
-      />
-      <ProjectItem
-        imageSrc={BookingIMG}
-        projectTitle="Booking"
-        projectDescription="One of several projects I did at the beginning of my education. The project was done in HTML and CSS."
-        projectLink="https://pmatyjasik.github.io/Booking/"
-        githubLink="https://github.com/pmatyjasik/Booking"
-      />
+      {ProjectsData.data.map((project, index) => (
+        <ProjectItem
+          imageSrc={project.imageSrc}
+          projectTitle={project.projectTitle}
+          projectDescription={project.projectDescription}
+          projectLink={project.projectLink}
+          githubLink={project.githubLink}
+          disabled={project.disabled}
+          key={`${index}-${project.projectTitle}`}
+        />
+      ))}
     </section>
   );
 };
