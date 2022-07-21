f19eb4b6433fcebd368cd18c2569a6602738acec
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-22 01:10:27 +0200

    Change technology subtitle

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -12,11 +12,7 @@ import Icon from "components/Icon";
 
 const Technologies = () => {
   return (
-    <WideSection
-      subheading="TECHNOLOGIES MASTERED"
-      heading="Technologies"
-      id="technologies"
-    >
+    <WideSection subheading="SKILLS" heading="Technologies" id="technologies">
       <Icon Icon={<SiJavascript />} className="mb-4 rounded md:mb-0" />
       <Icon Icon={<SiTypescript />} className="mb-4 md:mb-0" />
       <Icon Icon={<SiPhp />} className="mb-4 md:mb-0" />
