ba1a136a6c34afed726ad3f7185163bd6cf7d045
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-22 01:07:21 +0200

    Refactor component && add useState && add Icon

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,30 +1,38 @@
+import { useState } from "react";
 import { motion } from "framer-motion";
 import NavItem from "components/NavItem";
-
-interface Props {
-  isOpen: boolean;
-}
+import { FaBars } from "react-icons/fa";
 
 const variants = {
   open: { x: 0, transition: { type: "linear" } },
   closed: { x: "100%", transition: { type: "linear" } },
 };
-const MobileMenu = ({ isOpen }: Props) => {
+const MobileMenu = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const toggle = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
-    <motion.div
-      animate={isOpen ? "open" : "closed"}
-      initial="closed"
-      variants={variants}
-      id="burger_menu"
-      className="fixed right-0 z-10 flex flex-col items-center w-2/5 rounded-bl-[200px] rounded-tl-[50px] bg-primary top-15 md:hidden"
-    >
-      <ul className="py-10">
-        <NavItem isOpen={true} url="#about" text="About" />
-        <NavItem isOpen={true} url="#technologies" text="Technologies" />
-        <NavItem isOpen={true} url="#portfolio" text="Portfolio" />
-        <NavItem isOpen={true} url="#contact" text="Contact" />
-      </ul>
-    </motion.div>
+    <>
+      <FaBars
+        className={`z-50 w-8 h-8 text-white cursor-pointer md:hidden`}
+        onClick={toggle}
+      />
+      <motion.div
+        animate={isOpen ? "open" : "closed"}
+        initial="closed"
+        variants={variants}
+        id="burger_menu"
+        className="fixed right-0 z-10 flex flex-col items-center w-2/5 rounded-bl-[200px] rounded-tl-[50px] bg-primary top-15 md:hidden"
+      >
+        <ul className="py-10" onClick={toggle}>
+          <NavItem navMobile={true} url="#about" text="About" />
+          <NavItem navMobile={true} url="#technologies" text="Technologies" />
+          <NavItem navMobile={true} url="#portfolio" text="Portfolio" />
+          <NavItem navMobile={true} url="#contact" text="Contact" />
+        </ul>
+      </motion.div>
+    </>
   );
 };
 export default MobileMenu;
