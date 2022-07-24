9a7d857c894e36eb7a96757563e16140aaed0846
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-07-24 14:46:53 +0200

    Format code, change way of return

--- src/hooks/useCloseComponent.ts
+++ src/hooks/useCloseComponent.ts
@@ -1,34 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-export function useCloseComponent(
-  ref: React.RefObject<HTMLElement>
-){
+export function useCloseComponent(ref: React.RefObject<HTMLElement>) {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const handleCloseMenu = () => {
-    setIsOpen((prev) => !prev);
-  }
 
   useEffect(() => {
     function handleClickOutside(event: MouseEvent) {
-      if (ref.current && !ref.current.contains(event.target as Node) && isOpen) {
-        handleCloseMenu();
+      if (
+        ref.current &&
+        !ref.current.contains(event.target as Node) &&
+        isOpen
+      ) {
+        setIsOpen((prev) => !prev);
       }
     }
 
     function handleOnScroll(event: Event) {
-        if (ref.current && isOpen) {
-          handleCloseMenu();
-        }
+      if (ref.current && isOpen) {
+        setIsOpen((prev) => !prev);
+      }
     }
 
-    document.addEventListener('mousedown', handleClickOutside);
-    document.addEventListener('scroll', handleOnScroll);
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("scroll", handleOnScroll);
 
     return () => {
-    document.removeEventListener('mousedown', handleClickOutside);
-    document.removeEventListener('scroll', handleOnScroll);
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("scroll", handleOnScroll);
     };
   }, [isOpen, ref]);
-  return {isOpen, setIsOpen}
+  return [isOpen, setIsOpen] as const;
 }
