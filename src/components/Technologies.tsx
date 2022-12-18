cffeb7457b62a44b97647183326cff7d70042d70
PiotrMatyjasik <matyjasik.piotr@wp.pl>
2022-12-18 14:28:15 +0100

    Change php to next.js in technologies

--- src/components/Technologies.tsx
+++ src/components/Technologies.tsx
@@ -5,7 +5,7 @@ import {
   SiJavascript,
   SiReact,
   SiGit,
-  SiPhp,
+  SiNextdotjs,
 } from "react-icons/si";
 import WideSection from "components/WideSection";
 import Icon from "components/Icon";
@@ -15,7 +15,7 @@ const Technologies = () => {
     <WideSection subheading="SKILLS" heading="Technologies" id="technologies">
       <Icon Icon={<SiJavascript />} className="mb-4 rounded md:mb-0" />
       <Icon Icon={<SiTypescript />} className="mb-4 md:mb-0" />
-      <Icon Icon={<SiPhp />} className="mb-4 md:mb-0" />
+      <Icon Icon={<SiNextdotjs />} className="mb-4 md:mb-0" />
       <Icon Icon={<SiReact />} className="mb-4 md:mb-0" />
       <Icon Icon={<SiTailwindcss />} className="mb-4 md:mb-0" />
       <Icon Icon={<SiGit />} className="mb-4 md:mb-0" />
